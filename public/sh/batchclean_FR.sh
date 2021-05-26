5de8ac9c8f4a5636eb3a8d489c7f4c025635c919
Iris <812585367@qq.com>
2021-05-26 16:24:03 +0800

    "commit"

--- public/sh/batchclean_FR.sh
+++
@@ -1,6 +0,0 @@
-# 设置系统环境变量。
-source /etc/profile
-# 设置oracle用户的环境变量。
-source /oracle/.bash_profile
-# 执行
-/htidc/public/c/batchclean_FR /htidc/public/c/cleanarg.xml /log/publiclog/batchclear_FR.log 
